8cffb007347ceed2c22adfbe918d81e4e142fe20
Supriyo <72860690+Supsource@users.noreply.github.com>
2022-01-31 21:27:42 +0530

    Create Reorder List.cpp

---
+++ _Day 11 to 30/Stacks and Queues/Problems/Reorder List.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if ((!head) || (!head->next) || (!head->next->next)) return; // Edge cases
+        
+        stack<ListNode*> my_stack;
+        ListNode* ptr = head;
+        int size = 0;
+        while (ptr != NULL) // Put all nodes in stack
+        {
+            my_stack.push(ptr);
+            size++;
+            ptr = ptr->next;
+        }
+        
+        ListNode* pptr = head;
+        for (int j=0; j<size/2; j++) // Between every two nodes insert the one in the top of the stack
+        {
+            ListNode *element = my_stack.top();
+            my_stack.pop();
+            element->next = pptr->next;
+            pptr->next = element;
+            pptr = pptr->next->next;
+        }
+        pptr->next = NULL;
+    }
+};
