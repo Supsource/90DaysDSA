9b7653b98dd02fb071c4d5c5f32fffe5f868dadf
Supriyo <72860690+Supsource@users.noreply.github.com>
2022-02-01 15:00:40 +0530

    remove element

---
+++ _Day 11 to 30/Arrays/Problems/Remove Element.cpp
@@ -0,0 +1,12 @@
+class Solution { 
+    public:
+        int removeElement(vector<int>& nums, int val) {
+            int index = 0;
+            for (int i = 0; i < nums.size(); ++i) {
+                if (nums[i] != val) {
+                    nums[index++] = nums[i];
+                }
+            }
+            return index;
+        }
+    };
