57cc0c7e2025ded11f01636e9dba767305c0123d
Supriyo <72860690+Supsource@users.noreply.github.com>
2022-02-01 15:04:36 +0530

    unique paths II

---
+++ LeetCode Problems/Unique Paths II.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
+        int m = obstacleGrid.size() , n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        dp[0][1] = 1;
+        for(int i = 1 ; i <= m ; ++i)
+            for(int j = 1 ; j <= n ; ++j)
+                if(!obstacleGrid[i-1][j-1])
+                    dp[i][j] = dp[i-1][j]+dp[i][j-1];
+        return dp[m][n];
+    }
+};
