c84120a9a7ce773ab27005efc34aea6128f62825
Supriyo <72860690+Supsource@users.noreply.github.com>
2022-02-01 15:01:06 +0530

    search in rotated sorted array

---
+++ _Day 11 to 30/Arrays/Problems/Search in Rotated Sorted Array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int search(int A[], int n, int target) {
+        int lo=0,hi=n-1;
+        
+        while(lo<hi){
+            int mid=(lo+hi)/2;
+            if(A[mid]>A[hi]) lo=mid+1;
+            else hi=mid;
+        }
+        // lo==hi is the index of the smallest value and also the number of places rotated.
+        int rot=lo;
+        lo=0;hi=n-1;
+        // The usual binary search and accounting for rotation.
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            int realmid=(mid+rot)%n;
+            if(A[realmid]==target)return realmid;
+            if(A[realmid]<target)lo=mid+1;
+            else hi=mid-1;
+        }
+        return -1;
+    }
+};
