8ab66065ee5fc0b76ba9acb7a0beb1f82f4b848e
Supsource <supriyomalstartup@gmail.com>
2024-01-01 11:57:21 +0530

    Create Check Armstrong.cpp

---
+++ A2Z/01-Learn the basics/1.4-Know Basic Maths/Check Armstrong.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool ArmstrongNumber(int n)
+{
+    int originalno = n;
+    int count = 0;
+    int temp = n;
+    while (temp != 0)
+    {
+        count++;
+        temp = temp / 10;
+    }
+    int sumofpower = 0;
+    while (n != 0)
+    {
+        int digit = n % 10;
+        sumofpower += pow(digit,count);
+        n /= 10;
+    }
+    return (sumofpower == originalno);
+}
+int main()
+{
+    int n1 = 153;
+    if (ArmstrongNumber(n1))
+    {
+        cout << "Yes, it is an Armstrong Number\n";
+    }
+    else
+    {
+        cout << "No, it is not an Armstrong Number\n";
+    }
+    return 0;
+}
+
+// Problem link : https://www.codingninjas.com/studio/problems/check-armstrong_589?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
