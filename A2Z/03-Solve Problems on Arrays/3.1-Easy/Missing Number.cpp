16cf6967eee20449e6a371b03ece558fb0cccf0c
Supriyo <supriyomalstartup@gmail.com>
2024-01-04 11:13:21 +0530

    Create Missing Number.cpp

---
+++ A2Z/03-Solve Problems on Arrays/3.1-Easy/Missing Number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans=0;
+        for(int i = 0; i<nums.size(); i++){
+            ans^=nums[i];
+            ans^=i+1;
+        }
+        return ans;
+    }
+};
